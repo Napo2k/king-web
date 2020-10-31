da173391b57da3fb1f1786f954d472e80b086b94
sital anadkat <sitalanadkat@hotmail.com>
2020-10-31 18:59:52 +0000

    working webapp and tests

---
+++ web-hello-world/src/test/java/org/gradle/examples/web/ServletTest.java
@@ -0,0 +1,44 @@
+package org.gradle.examples.web;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.when;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+
+public class ServletTest {
+
+    @Mock
+    public HttpServletRequest request;
+    @Mock
+    public HttpServletResponse response;
+
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void doGet() throws Exception {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        when(response.getWriter()).thenReturn(printWriter);
+        new Servlet().doGet(request, response);
+        assertEquals("hello, world\n", stringWriter.toString());
+    }
+
+}
